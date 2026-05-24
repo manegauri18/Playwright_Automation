6e4ae9f21497949d943493a5f0bca9b605c8b044
Gauri <manegauri18@gmail.com>
2026-05-24 11:36:26 +0530

    jenkins test files

---
+++ Interview/tests/module1/demo.spec.js
@@ -0,0 +1,18 @@
+import{test, expect} from "@playwright/test"
+
+
+// Page Screenshot
+test("@sanity TC01", async({})=>{
+
+   console.log("sanity test executed from module1")
+})
+
+test("@smoke TC02", async({})=>{
+
+   console.log("smoke test executed from module1")
+})
+
+test("@regression TC03", async({})=>{
+
+   console.log("regression test executed from module1")
+})
---
+++ Interview/tests/module2/demo2.spec.js
@@ -0,0 +1,18 @@
+import{test, expect} from "@playwright/test"
+
+
+// Page Screenshot
+test("@sanity TC01", async({})=>{
+
+   console.log("sanity test executed from module2")
+})
+
+test("@smoke TC02", async({})=>{
+
+   console.log("smoke test executed")
+})
+
+test("@sanity TC03", async({})=>{
+
+   console.log("sanity test executed from module2")
+})
